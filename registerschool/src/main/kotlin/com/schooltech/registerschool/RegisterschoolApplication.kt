559009ff4810a2f20e42fa6f5cd375eb1837a059
Iury Amaral <iurydrayton@gmail.com>
2021-04-25 21:55:53 -0300

    Creation of register school microservice

---
+++ registerschool/src/main/kotlin/com/schooltech/registerschool/RegisterschoolApplication.kt
@@ -0,0 +1,11 @@
+package com.schooltech.registerschool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RegisterschoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<RegisterschoolApplication>(*args)
+}
---
+++ registerschool/src/test/kotlin/com/schooltech/registerschool/RegisterschoolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.schooltech.registerschool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RegisterschoolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
